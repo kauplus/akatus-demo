78091939ef6b2f6141cb258b7c0774e94974da42
Rafael Barbolo <rafael.barbolo@infosimples.com.br>
2013-10-25 15:14:48 -0200

    rails new akatus-demo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AkatusDemo::Application.config.secret_key_base = 'dac11304b6ac440aad7fb5ac3f7bffcf21eab041f081be1887e4635fa48563b7e690c3bbd26aff3e9704102a66fa89a8f984e4a5a69b1a272d063fa1cdb4ceab'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AkatusDemo::Application.config.session_store :cookie_store, key: '_akatus-demo_session'
